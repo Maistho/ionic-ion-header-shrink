00a63cb51975cc1d529ebd62452c2e721ca9ee5b
Gustav Bylund <maistho@gmail.com>
2015-05-08 00:10:38 +0200

    remove unused stuff

--- ionic.headerShrink.js
+++ ionic.headerShrink.js
@@ -1,59 +1,41 @@
+/* globals ionic */
 angular.module('ionic.ion.headerShrink', [])
 
-.directive('headerShrink', function($document) {
-  var fadeAmt;
+.directive('headerShrink', ['$document', function($document) {
+    return {
+        restrict: 'A',
+        link: function($scope, $element) {
+            var y = 0;
+            var prevY = 0;
+            var scrollDelay = 0.4;
 
-  var shrink = function(header, content, amt, max) {
-    amt = Math.min(max, amt);
-    fadeAmt = 1 - amt / max;
-    ionic.requestAnimationFrame(function() {
-      header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + amt + 'px, 0)';
-      for(var i = 0, j = header.children.length; i < j; i++) {
-        header.children[i].style.opacity = fadeAmt;
-      }
-    });
-  };
+            var fadeAmt;
 
-  return {
-    restrict: 'A',
-    link: function($scope, $element, $attr) {
-      var starty = $scope.$eval($attr.headerShrink) || 0;
-      var shrinkAmt;
+            var header = $document[0].body.querySelector('.search-bar');
+            var headerHeight = header.offsetHeight;
 
-      var amt;
+            function onScroll(e) {
+                var scrollTop = e.detail.scrollTop;
 
-      var y = 0;
-      var prevY = 0;
-      var scrollDelay = 0.4;
+                if (scrollTop >= 0) {
+                    y = Math.min(headerHeight / scrollDelay, Math.max(0, y + scrollTop - prevY));
+                } else {
+                    y = 0;
+                }
+                console.log(scrollTop);
 
-      var fadeAmt;
-      
-      var header = $document[0].body.querySelector('.bar-header');
-      var headerHeight = header.offsetHeight;
-      
-      function onScroll(e) {
-        var scrollTop = e.detail.scrollTop;
+                ionic.requestAnimationFrame(function() {
+                    fadeAmt = 1 - (y / headerHeight);
+                    header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, ' + -y + 'px, 0)';
+                    for (var i = 0, j = header.children.length; i < j; i++) {
+                        header.children[i].style.opacity = fadeAmt;
+                    }
+                });
 
-        if(scrollTop >= 0) {
-          y = Math.min(headerHeight / scrollDelay, Math.max(0, y + scrollTop - prevY));
-        } else {
-          y = 0;
+                prevY = scrollTop;
+            }
+
+            $element.bind('scroll', onScroll);
         }
-        console.log(scrollTop);
-
-        ionic.requestAnimationFrame(function() {
-          fadeAmt = 1 - (y / headerHeight);
-          header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, ' + -y + 'px, 0)';
-          for(var i = 0, j = header.children.length; i < j; i++) {
-            header.children[i].style.opacity = fadeAmt;
-          }
-        });
-
-        prevY = scrollTop;
-      }
-
-      $element.bind('scroll', onScroll);
-    }
-  }
-})
-
+    };
+}]);
